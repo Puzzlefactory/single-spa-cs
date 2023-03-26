f524ba97c50695ceafe4901e1c7b248f551632d0
PuzzleFactory <alan@oni.media>
2023-03-26 17:25:21 -0400

    Added files and made changes to configure Angular application to work as an MFE

--- vehicles/src/app/app-routing.module.ts
+++ vehicles/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '**', component: EmptyRouteComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
